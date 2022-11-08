94160dc441c90d0ce25341c4b63c8f1a6fd2d275
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2022-11-08 11:22:05 +0100

    Setting up AEM test context

--- core/src/test/java/com/adobe/aem/guides/wknd/core/models/impl/BylineImplTest.java
+++ core/src/test/java/com/adobe/aem/guides/wknd/core/models/impl/BylineImplTest.java
@@ -2,14 +2,23 @@ package com.adobe.aem.guides.wknd.core.models.impl;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import io.wcm.testing.mock.aem.junit5.AemContext;
+import io.wcm.testing.mock.aem.junit5.AemContextExtension;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+@ExtendWith(AemContextExtension.class)
 public class BylineImplTest {
 
-    @BeforeEach
-    void setUp() throws Exception {
+    private final AemContext ctx = new AemContext();
 
+    @BeforeEach
+    public void setUp() throws Exception {
+        //  registers the Sling Model to be tested
+        ctx.addModelsForClasses(BylineImpl.class);
+        // loads resource structures into the mock context
+        ctx.load().json("/com/adobe/aem/guides/wknd/core/models/impl/BylineImplTest.json", "/content");
     }
 
     @Test
