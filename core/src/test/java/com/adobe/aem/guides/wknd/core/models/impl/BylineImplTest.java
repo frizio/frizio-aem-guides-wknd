5fbdad810388c9f04e98ba09a3f19e37dbdc3faf
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2022-11-08 11:11:59 +0100

    Creating the JUnit test

---
+++ core/src/test/java/com/adobe/aem/guides/wknd/core/models/impl/BylineImplTest.java
@@ -0,0 +1,30 @@
+package com.adobe.aem.guides.wknd.core.models.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BylineImplTest {
+
+    @BeforeEach
+    void setUp() throws Exception {
+
+    }
+
+    @Test
+    void testGetName() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testGetOccupations() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testIsEmpty() {
+        fail("Not yet implemented");
+    }
+
+}
